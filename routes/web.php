2490e3a3c1396fbf9df51a0b54faf4ec283dc3d0
zis <azaiskr305@students.unnes.ac.id>
2023-07-13 07:52:01 +0700

    ngerapihin code + bikin layoutting

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('baseLayout');
+// });
+
 Route::get('/', function () {
-    return view('landing_page');
+    return view('layout/home');
 });
