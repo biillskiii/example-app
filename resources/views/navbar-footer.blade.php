db506e6272149b669b6a2298e49feff438207a35
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2023-07-14 00:36:30 +0700

    pakek layout

--- resources/views/navbar-footer.blade.php
+++ resources/views/navbar-footer.blade.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/navbar-footer.css') }}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/header.css') }}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/homeLayout.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/keanggotaan.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
