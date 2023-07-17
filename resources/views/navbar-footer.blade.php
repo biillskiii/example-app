4bd799a1c9afca00a0217fc17fafc0008de509b9
IlhamM3 <ilhammaulana05303005@gmail.com>
2023-07-16 16:35:52 -0800

    Nambah animasi, tentang, tujuan,divisi

--- resources/views/navbar-footer.blade.php
+++ resources/views/navbar-footer.blade.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/header.css') }}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/homeLayout.css') }}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/keanggotaan.css') }}">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
@@ -87,6 +88,10 @@
         });
 
     </script>
+     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+     <script>
+       AOS.init();
+     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
