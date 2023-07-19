7fc6a1a4ee31891f78eae5dfae04fe81e0d8cba8
biillskiii <muhamad.nabil20052003@gmail.com>
2023-07-19 17:13:43 +0700

    adding UnitTest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,9 +10,15 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_homepage(): void
     {
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+    public function test_keanggotaan(): void
+    {
+        $response = $this->get('/keanggotaan');
 
         $response->assertStatus(200);
     }
