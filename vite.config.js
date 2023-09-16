8394eb33a610d9901d00d796ccf5cde82169ff7a
zis <azaiskr305@students.unnes.ac.id>
2023-09-16 23:10:43 +0700

    change: vite.comfig

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['public/assets','public/css','public/js','public/picture'],
+            input: [
+            'public/css/acara-informasi.css','public/css/header.css','public/css/homeLayout.css','public/css/karya.css','public/css/keanggotaan.css','public/css/navbar-footer.css',
+            'public/js/aos.js','public/js/arrow.js','public/js/keanggotaan.js','public/js/landing.js'
+            ],
             refresh: true,
         }),
     ],
